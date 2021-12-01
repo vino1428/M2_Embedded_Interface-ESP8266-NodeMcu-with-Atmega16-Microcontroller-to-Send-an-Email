4ce5e648da98a360dcd25295d0988fcd4b2f195a
vino1428 <94214304+vino1428@users.noreply.github.com>
2021-12-01 16:35:34 +0530

    Create wifi.c

---
+++ 3_Implementation/scr/wifi.c
@@ -0,0 +1,14 @@
+#include <ESP8266WiFi.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+//Your WiFi Credentials
+// Set password to “ “ for open networks
+
+String apiKey = “ “;
+const char *ssid = “ “;
+const char *pass = “ “;
+const char *server = “Android”;
+© 2021 GitHub, Inc.
