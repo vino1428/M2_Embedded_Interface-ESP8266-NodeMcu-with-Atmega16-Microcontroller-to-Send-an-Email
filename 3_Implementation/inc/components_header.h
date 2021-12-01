b9c1c06b80682f5908fc98480c1ab347ebd30cd9
vino1428 <94214304+vino1428@users.noreply.github.com>
2021-12-01 14:36:09 +0530

    Create components_header.h

---
+++ 3_Implementation/inc/components_header.h
@@ -0,0 +1,32 @@
+// ADC Header
+#ifndef ADC_H
+#define ADC_H
+
+#include “includes.h”
+#endif
+
+// Delay Header
+#ifndef DELAY_H
+#define DELAY_H
+
+#include “includes.h”
+#endif
+
+// LCD Header
+#ifndef LCD_H
+#define LCD_H
+
+#include “includes.h”
+#define OUTPUT    0XFF
+#define LCD_DOT  0XFF
+#define LCD_SPC   0XFF
+
+#endif
+
+// Send SMS
+#ifndef SMS_H
+#define SMS_H
+
+#include “includes.h”
+
+#endif
